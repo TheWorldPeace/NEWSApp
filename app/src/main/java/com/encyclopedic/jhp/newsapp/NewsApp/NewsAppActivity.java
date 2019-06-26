c29b4134df34962573044552c6dc7e49e7b07341
Jianghp92 <1508168072@qq.com>
2019-06-26 11:22:55 +0800

    初始化项目20190626

---
+++ app/src/main/java/com/encyclopedic/jhp/newsapp/NewsApp/NewsAppActivity.java
@@ -0,0 +1,19 @@
+package com.encyclopedic.jhp.newsapp.NewsApp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ *
+ * 20190626
+ * 用于fragment和okhttp构造类似hupu的新闻app
+ * 参考链接 ：https://www.jianshu.com/p/9334b13350af
+ * use ViewPager,TabLayout,FragmentPagerAdapter,Fragment
+ */
+public class NewsAppActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
